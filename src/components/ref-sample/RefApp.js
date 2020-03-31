32b284dec92963271dccf0db908c9434aba4deae
jonggi2004 <jonggi2004@daum.net>
2020-03-31 18:05:12 +0900

    react-icon 추가

--- src/components/ref-sample/RefApp.js
+++ src/components/ref-sample/RefApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ScrollBoxApp from './ScrollBoxApp';
 import RefSample from './RefSample';
 import styled from 'styled-components';
+import { DiAndroid } from "react-icons/di";
 
 // 스타일 블럭은 반드시 대문자로 시작
 const RefAppBlock = styled.div`
@@ -15,7 +16,8 @@ const RefAppBlock = styled.div`
 const RefApp = () => {
     return (
         <RefAppBlock>
-            <h2>Ref Dom 지정하기</h2>
+            <h2><DiAndroid /> Ref Dom 지정하기</h2>
+            <p>(SVG Icon 적용: https://react-icons.netlify.com)</p>
             <p>Dom의 focus하거나 height 같은 정보를 알기위해 레퍼런스 지정이 필요하다.</p>
             <p>height 같은 정보는 render가 종료된 후 얻어야 하므로, LifeCycle API를 사용해야함.</p>
             <hr />
